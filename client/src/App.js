af9281caa7a9bc3a3c08043cb949659ad6f7121e
mcast045 <mcastaneda42@gmail.com>
2019-11-21 09:45:29 -0800

    Redux store setup

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,24 @@ import Navbar from './Components/layout/Navbar';
 import Landing from './Components/layout/landing';
 import Register from './Components/auth/register';
 import Login from './Components/auth/login';
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 const App = () =>
-  <BrowserRouter>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route path='/register' component={Register} />
-          <Route path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route path='/register' component={Register} />
+            <Route path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 
 export default App;
--- client/src/Components/auth/register.js
+++ client/src/Components/auth/register.js
@@ -38,8 +38,8 @@ const Register = () => {
                     <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
                     <small className="form-text"
                     >This site uses Gravatar so if you want a profile image, use a
-              Gravatar email</small
-                    >
+                        Gravatar email
+                    </small>
                 </div>
                 <div className="form-group">
                     <input
